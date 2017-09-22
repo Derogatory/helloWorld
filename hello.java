df01b3b49c9fe13f69efde1ffe44a20f1c2bfe30
George <george.ii@hotmail.com>
2017-09-22 05:09:15 -0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,11 +1,7 @@
 //This file is copywritten by George Nadarevic and cannot be used without form permission given by said party, any use of this code without explicit permission can be used against the offender in a court of law.
 
-public class hello
-	{
-		public static void main (String [] args)
-	
-		{
+public class hello {
+		public static void main (String [] args) {
 			System.out.println ("hello");
 		}
-	
 	}
